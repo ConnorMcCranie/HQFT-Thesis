8e5e793784db6193cf688762cd142c906589dd1a
ConnorMcCranie <comc6405@colorado.edu>
2024-07-15 16:06:44 -0600

    Added functionality for matrices

--- Code/hyperfunction_integration.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import scipy as sp
-import matplotlib.pyplot as plt
-import cmath
-
-from numpy import exp, sqrt, pi
-from scipy import integrate
-
-result = integrate.trapezoid(lambda x: x**2, 0, 1)
